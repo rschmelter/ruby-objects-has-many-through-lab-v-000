ca34a2670431045f1e501e3b55cdd083305ba15d
Ryan Schmelter <rschmelter33@gmail.com>
2017-12-09 18:39:27 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,8 +4,11 @@ class Patient
 
   def initialize(name)
     @name = name
+    @appointments = appointment
   end
 
-
+  def add_appointment(appointment)
+    @appointments << appointment
+  end
 
 end
