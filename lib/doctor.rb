c8ca2668978b9dc3013129b149b760f2211f40dc
Ryan Schmelter <rschmelter33@gmail.com>
2017-12-09 18:22:21 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,8 +8,8 @@ class Doctor
 
 
 
-def add_appointment(appointment)
-  @appointments << appointment
-  appointment.doctor = self
-end
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
 end
